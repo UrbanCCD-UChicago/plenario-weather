1c8ceca58336a587d88fc10eb070e295c93efabb
Manasi Thakkar <mat185@Pitt.edu>
2017-05-10 11:44:20 -0500

    download zip file

--- plenario_weather/tasks.py
+++ plenario_weather/tasks.py
@@ -1,3 +1,10 @@
-def download_hourly(datetime: str):
+import urllib.request
+#import zipfile,io
 
-    pass
+def download_hourly(datetime):
+	fileName = datetime.split("-")
+	fileName = "QCLCD" + fileName[0] + fileName[1] + ".zip";
+	file = urllib.request.urlopen("https://www.ncdc.noaa.gov/orders/qclcd/").read()
+	open(fileName, 'wb').write(file)
+
+    
